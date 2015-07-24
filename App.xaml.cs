73bb92c37e0903ebd3f8628769de16ce4eedb7d8
David <dave07747@gmail.com>
2015-07-24 15:34:56 -0400

    Search for parts through tower
    A project for Mr. Paterno

---
+++ App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TowerSearch
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            EventManager.RegisterClassHandler(typeof(TextBox), TextBox.GotFocusEvent, new RoutedEventHandler(TextBox_GotFocus));
+
+            EventManager.RegisterClassHandler(typeof(TextBox), TextBox.PreviewMouseDownEvent, new RoutedEventHandler(TextBox_PreviewMouseDown));
+
+            base.OnStartup(e);
+
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+            (sender as TextBox).SelectAll();
+
+        }
+
+        private void TextBox_PreviewMouseDown(object sender, RoutedEventArgs e)
+        {
+
+            TextBox textBox = sender as TextBox;
+
+            if (!textBox.IsFocused)
+            {
+
+                textBox.Focus();
+
+                textBox.SelectAll();
+
+                e.Handled = true;
+
+            }
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace TowerSearch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void partName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void towerNumber_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void rowX_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void columnY_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}

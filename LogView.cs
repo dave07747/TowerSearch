50882754340c9d1d792f272f8738d5f33948309e
David <dave07747@gmail.com>
2015-08-21 11:52:40 -0400

    Database
    Added database and table for log and partlog

---
+++ LogView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TowerSearch
+{
+    public partial class LogView : Form
+    {
+        public LogView()
+        {
+            InitializeComponent();
+            DataClasses1DataContext databaseLogs = new DataClasses1DataContext();
+            dataGridView1.DataSource = databaseLogs.Logs;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -50,5 +50,10 @@ namespace TowerSearch
         {
 
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            new LogView().Show();
+        }
     }
 }

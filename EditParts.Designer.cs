852fa851fe7e1b3c92fa2f1af8fb459465a9f69e
David <dave07747@gmail.com>
2015-09-02 11:32:19 -0400

    Added edit parts

--- EditParts.Designer.cs
+++ EditParts.Designer.cs
@@ -121,14 +121,12 @@
             // 
             // button1
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
             this.button1.Location = new System.Drawing.Point(558, 0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(53, 113);
+            this.button1.Size = new System.Drawing.Size(53, 336);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Add Row";
+            this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
--- EditParts.cs
+++ EditParts.cs
@@ -16,8 +16,13 @@ namespace TowerSearch
 {
     public partial class EditParts : Form
     {
-        string conString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=X:\TowerSearch\TowerSearch\Parts.mdf;Integrated Security=True";
-        DataClasses1DataContext PartsLog = new DataClasses1DataContext();
+        const string conString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=X:\TowerSearch\TowerSearch\Parts.mdf;Integrated Security=True";
+        static DataClasses1DataContext PartsLog = new DataClasses1DataContext(conString);
+        static Table<Part> listOfParts = PartsLog.GetTable<Part>();
+
+        SqlDataAdapter sda;
+        SqlCommandBuilder scb;
+        DataTable dt;
 
         public EditParts()
         {
@@ -28,37 +33,54 @@ namespace TowerSearch
             //Start binding
            
 
-            Table<Part> parts = PartsLog.GetTable<Part>();
+           /* Table<Part> parts = PartsLog.GetTable<Part>();
 
             var allParts = from Part in parts select Part;
 
             partBindingSource.DataSource = allParts;
 
-            dataGridView1.DataSource = partBindingSource;
+            dataGridView1.DataSource = partBindingSource;*/
 
         }
 
         private void showData()
         {
-            DataSet DS = new DataSet();
+           /* DataSet DS = new DataSet();
             SqlConnection con = new SqlConnection(conString);
             con.Open();
             SqlDataAdapter DA = new SqlDataAdapter("SELECT * FROM Parts", con);
             DA.Fill(DS, "Parts");
             dataGridView1.DataSource = DS.Tables["Parts"];
-            con.Close();
+            con.Close();*/
+
+            SqlConnection con = new SqlConnection(conString);
+            sda = new SqlDataAdapter("SELECT * FROM Parts", con);
+
+            dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
         }
 
         private void EditParts_Load(object sender, EventArgs e)
         {
-            showData();
+           showData();
+
+        
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            partBindingSource.EndEdit();
+            try
+            {
+                scb = new SqlCommandBuilder(sda);
+                sda.Update(dt);
 
-            PartsLog.SubmitChanges();
+                MessageBox.Show("Saved");
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
 
             showData();
         }

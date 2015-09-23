9592a2a826738e1038d03e6dd1c167f2e00c4603
David <dave07747@gmail.com>
2015-09-23 12:27:14 -0400

    Trying to make it work

--- obj/Release/MainWindow.g.cs
+++ obj/Release/MainWindow.g.cs
@@ -9,7 +9,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-using CustomWindow;
+
 using System;
 using System.Diagnostics;
 using System.Windows;
